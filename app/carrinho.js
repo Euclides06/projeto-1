2b6bff9ee813e4d7aa0a4b524fac28ca24ddea28
Alan Santos <alan.santos@al.infnet.edu.br>
2023-03-07 22:30:43 -0300

    Função de Remover itens do Carrinho

--- app/carrinho.js
+++ app/carrinho.js
@@ -7,7 +7,7 @@ for (var i = 0; i < keys.length; i++) {
     cart.push(JSON.parse(localStorage.getItem(keys[i])))
 }
 
-
+console.log(cart)
 
 
 for (let i = 0; i < cart.length; i++) {
@@ -15,6 +15,7 @@ for (let i = 0; i < cart.length; i++) {
                     <li>
                         <div class="imagem"><img src="${cart[i].imagemDestaque}" /></div>
                         <div class="infos-carrinho">
+                            <p style="display: none" class="nameSku">${cart[i].sku}</P>
                             <p class="nome-produto">${cart[i].nome}<span class="material-symbols-outlined botao-remover" id=${cart[i].sku}>delete</span></strong></p>
                             <div class="cor"><strong>Cor:</strong> ${cart[i].cores}</div>
                             <div class="tamanho"><strong>Tamanho:</strong> ${cart[i].tamanhos}</div>
@@ -29,3 +30,15 @@ for (let i = 0; i < cart.length; i++) {
                     </li>
             `
 }
+
+
+// REMOVER CARRINHO
+
+const removeProduct = document.querySelector('.botao-remover');
+removeProduct.addEventListener('click', function(){
+
+    let sku = document.querySelector('.nameSku').innerText
+    console.log(sku);
+    localStorage.removeItem(sku);
+    location.reload();
+})
